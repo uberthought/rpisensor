9e771cd04709ed73ff38f1e1071acd18bb4cf75a
root <uberthought@gmail.com>
2017-11-28 13:58:06 +0000

    implemented simple web interface

--- WebServer.py
+++ WebServer.py
@@ -7,6 +7,9 @@ import logging
 
 from Settings import Settings
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 #hostName = "ec2-13-58-202-229.us-east-2.compute.amazonaws.com"
 hostName = ''
 hostPort = 80
@@ -30,17 +33,18 @@ class WebServer(BaseHTTPRequestHandler):
         else:
             postvars = {}
 
-        logging.debug('TYPE %s' % (ctype))
-        logging.debug('PATH %s' % (self.path))
-        logging.debug('ARGS %d' % (len(postvars)))
+        logger.debug('TYPE %s' % (ctype))
+        logger.debug('PATH %s' % (self.path))
+        logger.debug('ARGS %d' % (len(postvars)))
 
         print(postvars)
 
-        if b'save' in postvars.keys():
-            # self.showRoot('tapped save')
-            self.saveButton(postvars)
-        else:
-            self.showRoot('unknown error')
+        if b'warmer' in postvars.keys():
+            self.warmerButton()
+        if b'cooler' in postvars.keys():
+            self.coolerButton()
+        if b'power' in postvars.keys():
+            self.powerButton()
 
 
     def showRoot(self, message):
@@ -56,31 +60,31 @@ class WebServer(BaseHTTPRequestHandler):
 
         self.wfile.write(bytes('<script>', 'utf-8'))
         self.wfile.write(bytes('document.getElementById("message").innerHTML ="' + message + '";', 'utf-8'))
-        self.wfile.write(bytes('document.getElementById("target").value ="' + str(settings.getTargetF()) + '";', 'utf-8'))
+        self.wfile.write(bytes('document.getElementById("target").innerHTML ="' + str(settings.getTargetF()) + '";', 'utf-8'))
         if settings.getOn():
-            self.wfile.write(bytes('document.getElementById("on").checked = true;', 'utf-8'))
+            self.wfile.write(bytes('document.getElementById("power").value ="Turn Off";', 'utf-8'))
         else:
-            self.wfile.write(bytes('document.getElementById("on").checked = false;', 'utf-8'))
+            self.wfile.write(bytes('document.getElementById("power").value ="Turn On";', 'utf-8'))
         self.wfile.write(bytes('</script>', 'utf-8'))
 
-    def saveButton(self, postvars):
-
-        logging.debug('saveButton')
-
+    def warmerButton(self):
+        logger.debug('warmerButton')
         settings = Settings()
-
-        if b'on' in postvars.keys():
-            settings.setOn(True)
-        else:
-            settings.setOn(False)
-
-        for item in postvars.items():
-            if item[0] == b'target':
-                target = (item[1][0]).decode("utf-8")
-                settings.setTargetF(target)
-
+        settings.setTargetF(settings.getTargetF() + 1)
         self.showRoot(self.getState())
-    
+
+    def coolerButton(self):
+        logger.debug('coolerButton')
+        settings = Settings()
+        settings.setTargetF(settings.getTargetF() - 1)
+        self.showRoot(self.getState())
+
+    def powerButton(self):
+        logger.debug('powerButton')
+        settings = Settings()
+        settings.setOn(not settings.getOn())
+        self.showRoot(self.getState())
+
     def getState(self):
         settings = Settings()
         message = 'Heater is '
@@ -89,10 +93,9 @@ class WebServer(BaseHTTPRequestHandler):
         else:
             message += 'off'
         
-        message += '</br>Target temperature is ' + str(settings.getTargetF())
+        # message += '</br>Current temperature is ' + str(settings.getTargetF())
         
         return message
-
 
 webServer = HTTPServer((hostName, hostPort), WebServer)
 
@@ -102,6 +105,6 @@ except KeyboardInterrupt:
     pass
 
 webServer.server_close()
-logging.info(time.asctime() + "Server Stops - %s:%s" % (hostName, hostPort))
+logger.info(time.asctime() + "Server Stops - %s:%s" % (hostName, hostPort))
 
 

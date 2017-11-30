5e0b903673928dd40acf1f8d3c36b294517755f7
root <uberthought@gmail.com>
2017-11-30 16:06:33 +0000

    fix blocking issue with WebServer class

--- Communication.py
+++ Communication.py
@@ -6,6 +6,8 @@ class Communication:
     def sendExperiences():
         if os.path.exists('experiences.p'):
             subprocess.call(['scp', '-P 2222', 'experiences.p', 'root@35.202.235.6:DQN/'])
+        # if os.path.exists('settings.p'):
+        #     subprocess.call(['scp', '-P 2222', 'settings.p', 'root@35.202.235.6:DQN/'])
     
     def getNetwork():
         if not os.path.isdir('train'):

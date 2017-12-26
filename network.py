fix off by one bug in value

network.py
@@ -19,7 +19,7 @@ class Model:
         self.states1 = tf.placeholder(tf.float32, shape=(None, self.state_size), name='states1')
         self.values = tf.placeholder(tf.float32, shape=(None, 1), name='values')
 
-        units = 4
+        units = 32
 
         model_input = tf.concat([self.states0, self.actions], axis=1)
         model_hidden0 = tf.layers.dense(inputs=model_input, units=units, activation=tf.nn.relu)
@@ -64,7 +64,7 @@ class Model:
         states1 = np.array([], dtype=np.float).reshape(0, self.state_size)
         values = np.array([], dtype=np.float).reshape(0, 1)
 
-        training_count = 10000
+        training_count = 100
         training_data = experiences.get()
         if len(training_data) > training_count:
             training_experiences = np.random.choice(training_data, training_count)
@@ -91,7 +91,7 @@ class Model:
         return loss
 
     def dqn_train(self, experiences):
-        discount_factor = 1
+        discount_factor = 0.5
 
         X = np.array([], dtype=np.float).reshape(0, self.states0.shape[1])
         Y = np.array([], dtype=np.float).reshape(0, self.action_size)
@@ -123,7 +123,7 @@ class Model:
                 actions0 = self.dqn_run(states0)
                 actions1 = self.dqn_run(states1)
                 action = [np.argmax(actions0)]
-                actions0 = [sigmoid(values[x] + discount_factor * np.max(actions1[x])) for x in range(self.action_size)]
+                actions0 = [values[x] + discount_factor * np.max(actions1[x]) for x in range(self.action_size)]
 
                 X = np.concatenate((X, np.reshape(state0, (1, self.state_size))), axis=0)
                 Y = np.concatenate((Y, np.reshape(actions0, (1, self.action_size))), axis=0)

online_train.py
@@ -31,17 +31,15 @@ action = 0
 
 while True:
     
-    simulation.step()
-
     if Settings.getOn():
 
         experience = experiences.getLast()
         state = experience.state0
         actions = model.dqn_run([state])
         action = np.argmax(actions)
-        if random.random() < 0.5:
-            # action = abs(action - 1)
-            action = np.random.choice(2, 1)[0]
+        if random.random() < 0.25:
+            action = abs(action - 1)
+            # action = np.random.choice(2, 1)[0]
 
         force = False
         if temperature < target - target_delta:
@@ -56,6 +54,7 @@ while True:
         else:
             solenoid.switchOn()
 
+        simulation.step()
         temperature, humidity, timestamp = sensor.gather()
         if not force:
             experiences.add(temperature, humidity, solenoid.isOn(), timestamp, target, target_delta)
@@ -70,6 +69,7 @@ while True:
         print(temperature * 9 / 5 + 32, state, action, 'actions', actions, 'value', experience.value, 'model', model_loss, 'dqn', dqn_loss)
 
     else:
+        simulation.step()
         temperature, humidity, timestamp = sensor.gather()
         print(temperature * 9 / 5 + 32)
 

plot3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import matplotlib
-matplotlib.use('Agg')
+# import matplotlib
+# matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,31 +25,56 @@ target_delta = experiences.experiences[-1].target_delta
 experiencesFake = Experiences()
 experiencesFake.experiences = []
 for temperature in np.arange(min, max, .01):
+        experiencesFake.add2(temperature, .5, True, 0, target, target_delta)
         experiencesFake.add2(temperature, .5, False, 0, target, target_delta)
 
-fooFake = experiencesFake.get()[2:]
+# fooFake = experiencesFake.get()[2:]
+fooFake = experiences.get()
 valuesOn = []
 valuesOff = []
+predictedOn = []
+predictedOff = []
 valuesDiff = []
-temperatures = []
+temperaturesOn = []
+temperaturesOff = []
 
 for experience in fooFake:
     state0 = experience.state0
-    _, values = model.model_run([state0, state0], [[0], [1]])
+    state1 = experience.state1
+    value = experience.value
+    action = experience.action
 
-    valuesOff.append(values[0][0])
-    valuesOn.append(values[1][0])
-    valuesDiff.append(values[1][0] - values[0][0])
-    temperatures.append(experience.state0[2])
+    states0 = [state0, state0]
+    actions = [[0], [1]]
+    states1, values = model.model_run(states0, actions)
 
-valuesOff = np.array(valuesOff) / 100
-valuesOn = np.array(valuesOn) / 100
+    if experience.action[0] == 0:
+        temperaturesOff.append(state0[2])
+        predictedOff.append(values[0][0])
+        valuesOff.append(value)
+        # valuesOff.append(values[0][0] - value)
+        # statesDiff.append(experience.state1[2] - states1[0][2])
+    else:
+        temperaturesOn.append(state0[2])
+        predictedOn.append(values[1][0])
+        valuesOn.append(value)
+        # valuesOn.append(values[1][0] - value)
+        # statesDiff.append(experience.state1[2] - states1[1][2])
+    
+
+# valuesDiff = np.array(valuesDiff) * 10
 
 fig, ax = plt.subplots(figsize=(20, 10))
 # fig, ax = plt.subplots()
-ax.plot(temperatures, valuesOn, '.', label='on', color='red')
-ax.plot(temperatures, valuesOff, '.', label='off', color='blue')
-ax.plot(temperatures, valuesDiff, '.', label='diff', color='gray')
+ax.plot(temperaturesOn, valuesOn, '.', label='actual on', color='red')
+ax.plot(temperaturesOff, valuesOff, '.', label='actual off', color='blue')
+ax.plot(temperaturesOn, predictedOn, '.', label='predicted on', color='gray')
+ax.plot(temperaturesOff, predictedOff, '.', label='predicted off', color='green')
+# ax.plot(temperatures, valuesOn, '.', label='on', color='red')
+# ax.plot(temperatures, valuesOff, '.', label='off', color='blue')
+# ax.plot(temperatures, valuesDiff, '.', label='on vs. off', color='gray')
+# ax.plot(temperatures, values2, '.', label='values', color='green')
+# ax.plot(temperatures, statesDiff, '.', label='values', color='green')
 
 legend = ax.legend(loc='lower right')
 for label in legend.get_lines():
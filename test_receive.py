e2f33efb598c214aadcc3e7ff7a4065274a19050
Philip Richardson <uberthought@gmail.com>
2018-05-24 07:07:48 -0400

    added test code for Communications

---
+++ test_receive.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from Communication import Communication
+from Experiences import Experiences
+import time
+
+communication = Communication()
+
+while True:
+    experiences = communication.receiveExperiences()
+    print(experiences)
+    time.sleep(1)
---
+++ test_send.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from Communication import Communication
+from Experiences import Experiences
+
+experiences = Experiences()
+communication = Communication()
+
+communication.sendExperiences(experiences)

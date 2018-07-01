5bea03c0fedd24623b8c831aec75d086f1a6f3ab
Philip Richardson <uberthought@gmail.com>
2018-07-01 13:46:17 -0400

    add umount helper

--- server/sshfs.sh
+++ server/sshfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "$DIRECTORY" ]; then
-    mkdir ~/server
+if [ ! -d "~/$DIRECTORY" ]; then
+    mkdir ~/$DIRECTORY
 fi
-sshfs uberthought@35.232.250.6:. ~/server
+sshfs uberthought@35.232.250.6:. ~/$DIRECTORY
